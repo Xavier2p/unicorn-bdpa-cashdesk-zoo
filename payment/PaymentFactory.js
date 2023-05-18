5d96694560f74c10e9aea632bf6d1d52911f6a98
Xavier2p <xavier.de-place@epita.fr>
2023-05-18 16:03:17 +0200

    Add price and reduction

--- payment/PaymentFactory.js
+++ payment/PaymentFactory.js
@@ -1,24 +1,14 @@
 class PaymentFactory {
-    createPayment(paymentType) {
-        if (paymentType === "BTC") {
-            return new BTCPaymentBuilder();
-        }
-        else if (paymentType === "Card") {
-            return new CardPaymentBuilder();
-        }
-        else if (paymentType === "Cash") {
-            return new CashPaymentBuilder();
-        }
-        else {
-            return new Error("Unknown payment type: " + paymentType);
-        }
+    constructor(price) { this.price = price; }
+
+    createPayment = (paymentType) => {
+        if (paymentType === "BTC") return new BTCPaymentBuilder();
+        else if (paymentType === "Card") return new CardPaymentBuilder();
+        else if (paymentType === "Cash") return new CashPaymentBuilder();
+        else return new Error("Unknown payment type: " + paymentType);
     }
 
-    applyReduction(reduction) {
-
-    }
+    applyReduction = (reduction) => { this.price = reduction.applyReduction(this.price) }
 }
 
-
-
-module.exports = {PaymentFactory};
+module.exports = { PaymentFactory };
---
+++ payment/Reduction.js
@@ -0,0 +1,20 @@
+class Reduction {
+    constructor(reduction) {
+        this.reduction = reduction;
+    }
+
+    applyReduction = (price) => {
+        return price - this.reduction;
+    }
+}
+
+class NullReduction {
+    constructor() {
+        this.reduction = 0;
+    }
+
+    applyReduction = (price) => {
+        return price;
+    }
+}
+module.exports = { Reduction, NullReduction };
