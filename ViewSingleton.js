b33351e4cd2a7f3c688f4df321aa22038dc2993c
Samuel PERRIER <samuel.perrier@epita.fr>
2023-05-19 11:43:27 +0200

    add view

---
+++ ViewSingleton.js
@@ -0,0 +1,17 @@
+const { database } = require('../DatabaseSingleton');
+
+class View {
+    constructor() {
+        if (View.view == null) {
+            View.view = this;
+        }
+        return View.view;
+    }
+
+    info() {
+        console.log("People on site: " + database.peopleOnSite);
+    }
+}
+
+const view = new View();
+module.exports = { View };
